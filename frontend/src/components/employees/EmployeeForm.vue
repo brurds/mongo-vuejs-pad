d68f3f5bcf3a4f3c3840b107d9598d74afe183aa
Bruno Rodrigues dos Santos <brurdsfac@gmail.com>
2020-04-29 16:39:03 -0300

    update

--- frontend/src/components/employees/EmployeeForm.vue
+++ frontend/src/components/employees/EmployeeForm.vue
@@ -10,8 +10,8 @@
               <InputText id="name" type="text" v-model.trim="$v.name.$model" />
               <label for="name">Digite o nome</label>
             </span>
-            <p class="error" v-if="validateRequired($v.name)">Campo necessario</p>
-            <p class="error" v-if="validateText($v.name)">Deve conter ao menos 3 caracteres</p>
+            <p class="error" v-if="validateRequired($v.name)">{{msg.required}}</p>
+            <p class="error" v-if="validadeMinLength($v.name)">{{msg.minLength}}</p>
           </div>
           <div class="p-col-6">
             <h3>Cargo</h3>
@@ -19,8 +19,8 @@
               <InputText id="position" type="text" v-model.trim="$v.position.$model" />
               <label for="position">Digite o cargo</label>
             </span>
-            <p class="error" v-if="validateRequired($v.position)">Campo necessario</p>
-            <p class="error" v-if="validateText($v.position)">Deve conter ao menos 3 caracteres</p>
+            <p class="error" v-if="validateRequired($v.position)">{{msg.required}}</p>
+            <p class="error" v-if="validadeMinLength($v.position)">{{msg.minLength}}</p>
           </div>
           <div class="p-col-6">
             <h3>Area Funcional</h3>
@@ -28,8 +28,8 @@
               <InputText id="functional" type="text" v-model.trim="$v.functional.$model" />
               <label for="functional">Digite a area</label>
             </span>
-            <p class="error" v-if="validateRequired($v.functional)">Campo necessario</p>
-            <p class="error" v-if="validateText($v.functional)">Deve conter ao menos 3 caracteres</p>
+            <p class="error" v-if="validateRequired($v.functional)">{{msg.required}}</p>
+            <p class="error" v-if="validadeMinLength($v.functional)">{{msg.minLength}}</p>
           </div>
           <div class="p-col-6">
             <h3>Registro</h3>
@@ -42,8 +42,9 @@
               />
               <label for="register">Digite o registro</label>
             </span>
-            <p class="error" v-if="this.validateRequired($v.register)">Campo necessario</p>
-            <p class="error" v-if="!$v.register.maxValue">Maximo de 9 digitos</p>
+            <p class="error" v-if="validateRequired($v.register)">{{msg.required}}</p>
+            <p class="error" v-if="validadeMinLength($v.register)">{{msg.minLength}}</p>
+            <p class="error" v-if="validadeMaxValue($v.register)">{{msg.maxValue}}</p>
           </div>
           <div class="p-col-align-center div-flex">
             <Button @click="save()" label="Salvar" class="p-button-info p-button-rounded btn-size" />
@@ -65,11 +66,17 @@ export default {
       position: "",
       functional: "",
       register: "",
+
       employee: {
         name: "",
         position: "",
         functional: "",
         register: ""
+      },
+      msg: {
+        required: "*Campo não pode estar vazio",
+        minLength: "*Necessario 3 carateres no minimo",
+        maxValue: "*Maximo de 9 caracteres"
       }
     };
   },
@@ -88,6 +95,7 @@ export default {
     },
     register: {
       required,
+      minLength: minLength(3),
       maxValue: maxValue(999999999)
     }
   },
@@ -103,8 +111,11 @@ export default {
     validateRequired(field) {
       return !field.required;
     },
-    validateText(field) {
+    validadeMinLength(field) {
       return !field.minLength && field.required;
+    },
+    validadeMaxValue(field) {
+      return !field.maxValue;
     },
     save() {
       if (this.$v.$invalid) {
